fbc19e2338ad97aef24aeb6cbe449744f6591b9f
Martin Cowey <starman.martin@gmail.com>
2025-01-18 22:56:24 +0000

    Add home page html css and javascript

--- unchained/settings.py
+++ unchained/settings.py
@@ -153,6 +153,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Ensure this matches your structure
+    os.path.join(BASE_DIR, 'home/static'),  # Include home app's static files
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
