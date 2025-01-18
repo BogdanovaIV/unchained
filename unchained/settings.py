d8fd5e4bece8db89184f943ba1278395e16c9914
Semi Akhigbe <akhigbejoy5@gmail.com>
2025-01-18 09:27:24 +0000

    Fix the issue with the static file and map

--- unchained/settings.py
+++ unchained/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
+ALLOWED_HOSTS =[
+
     os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
@@ -150,7 +151,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
